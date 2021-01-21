663cfa7f0ff34854a6f1d567599f944b148649ba
hitesh70738 <hitesh70738@gmail.com>
2021-01-21 16:10:30 +0000

    removed deactivate

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,7 +3,6 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 cd service_2
@@ -11,7 +10,6 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 cd service_3
@@ -19,7 +17,6 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
 
 cd service_4
@@ -27,5 +24,4 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
-deactivate
 cd ..
