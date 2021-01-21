14688bd34c1434a6b2b88ea8317aa6587f3bed62
hitesh70738 <hitesh70738@gmail.com>
2021-01-21 16:41:42 +0000

    yes

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,26 +1,22 @@
 cd service_1
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest service_1 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_2
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest service_2 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_3
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest service_3 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_4
-source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
 python3 -m pytest service_4 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
