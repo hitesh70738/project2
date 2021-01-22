74f4d68ee0ab43485322417d2994edcaae5f7872
hitesh70738 <hitesh70738@gmail.com>
2021-01-22 10:15:43 +0000

    removed services from pytest

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,23 +1,23 @@
 cd service_1
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
-python3 -m pytest service_1 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_2
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
-python3 -m pytest service_2 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_3
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
-python3 -m pytest service_3 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_4
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov application requests_mock
-python3 -m pytest service_4 --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
