0ec1ac4e1ea398949a961fdb0fb168d97186fb85
hitesh70738 <hitesh70738@gmail.com>
2021-01-22 14:13:15 +0000

    author

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,6 @@
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
+    export AUTHOR=${AUTHOR}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml car-project-stack
 EOF
