6c1647f93a65347d851050d475a8085bd9eeb9f5
hitesh70738 <hitesh70738@gmail.com>
2021-01-22 10:23:02 +0000

    all updated

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,23 +1,23 @@
 cd service_1
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov application requests_mock
+pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_2
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov application requests_mock
+pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_3
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov application requests_mock
+pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
 
 cd service_4
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov application requests_mock
+pip3 install pytest pytest-cov requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 cd ..
