f18c54c88962120afb0ba586642869cc7fe46718
hitesh70738 <hitesh70738@gmail.com>
2021-01-22 11:11:32 +0000

    yh

--- jenkins/build_images.sh
+++ jenkins/build_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl https://get.docker.com | sudo bas
+curl https://get.docker.com | sudo bash
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $(whoami)
 sudo usermod -aG docker jenkins
